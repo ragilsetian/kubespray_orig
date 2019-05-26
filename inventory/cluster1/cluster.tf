18319aaedc79e273628d8b9b249271374f8c330b
ragilsetian <38203279+ragilsetian@users.noreply.github.com>
2019-05-27 04:49:13 +0700

    Update cluster.tf

--- inventory/cluster1/cluster.tf
+++ inventory/cluster1/cluster.tf
@@ -1,7 +1,7 @@
 # Cluster test Provisioning
 
 cluster_name = "k8s"
-az_list=["jakarta-1a", "jakarta-1b", "jakarta-1c"]
+az_list=["jakarta-1a", "jakarta-1b", "jakarta-1c"] # availability zone list
 dns_nameservers=["8.8.8.8" , "8.8.4.4"]
 
 # SSH key to use for access to nodes
@@ -15,23 +15,23 @@ ssh_user = "ubuntu"
 
 # 0|1 bastion nodes
 number_of_bastions = 0
-flavor_bastion = "c1b7530d-be10-4097-a90f-d75423d97210"
+flavor_bastion = "ID flavor bastion node"
 
 # standalone etcds
 number_of_etcd = 0
-flavor_etcd = "c1b7530d-be10-4097-a90f-d75423d97210"
+flavor_etcd = "ID flavor etcd node"
 
 # masters
 number_of_k8s_masters = 1
 number_of_k8s_masters_no_etcd = 0
 number_of_k8s_masters_no_floating_ip = 0
 number_of_k8s_masters_no_floating_ip_no_etcd = 0
-flavor_k8s_master = "c1b7530d-be10-4097-a90f-d75423d97210"
+flavor_k8s_master = "ID flavor master node"
 
 # nodes
 number_of_k8s_nodes = 2
 number_of_k8s_nodes_no_floating_ip = 0
-flavor_k8s_node = "c1b7530d-be10-4097-a90f-d75423d97210"
+flavor_k8s_node = "ID flavor worker node"
 
 # GlusterFS
 # either 0 or more than one
@@ -45,7 +45,7 @@ image_gfs = "buntu 18.04 LTS"
 
 # networking
 network_name = "k8s-network"
-external_net = "fa2c3b23-5f25-4ab1-b06b-6edc405ec323"
-subnet_cidr = "10.20.20.0/24"
-floatingip_pool = "Public_Network"
+external_net = "ID external network"
+subnet_cidr = "subnet for new internal network"
+floatingip_pool = "name of external network"
 master_allowed_remote_ips = ["0.0.0.0/0"]
